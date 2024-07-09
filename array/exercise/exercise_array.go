3b6404b058c3d2ad2f307daef7b441971c6ee563
Jefferson Santos <jefferson.pereira-ext@olxbr.com>
2024-07-09 01:03:30 -0300

    feat: added one more exercise of string

--- array/exercise/exercise_array.go
+++ array/exercise/exercise_array.go
@@ -3,12 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(reverse("Hello World"))
+/*	fmt.Println(reverse("Hello World"))
 
 	first := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	second := []int{4, 4, 6, 22}
 
-	fmt.Println(mergeSortedArray(first, second))
+	fmt.Println(mergeSortedArray(first, second)) */
+
+	fmt.Println(strStr("sadbutsad", "sad"))
+	fmt.Println(strStr("leetcode", "leeto"))
+}
+
+// Exercise - Find the Index of the First Occurrence in a String
+func strStr(haystack string, needle string) int {
+	h := len(haystack)
+	n := len(needle)
+	for i := 0; i < (h-n+1); i++ {
+		if haystack[i:i+n] == needle {
+			return i
+		}
+	}
+	return -1
 }
 
 // Exercise 1 - reverse string
